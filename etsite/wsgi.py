ea771b082d5aded52cfa627742b9b5804030aa5c
Niu Guangyuan <niug@garena.com>
2016-02-22 18:50:50 +0800

    init project

---
+++ etsite/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for etsite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "etsite.settings")
+
+application = get_wsgi_application()
